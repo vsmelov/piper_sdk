8b09759edfc1f8c1d021296244a2668abfc2a0b6
ViTekVTECilla <korproftech@gmail.com>
2025-07-22 03:49:17 +0300

    fix

--- demo/V2/manage/disable_motor.py
+++ demo/V2/manage/disable_motor.py
@@ -2,10 +2,10 @@ from piper_sdk import *
 from demo.V2.settings import CAN_LEFT, CAN_RIGHT
 
 
-USE_CAN_NAME = CAN_LEFT
+USE_CAN_NAME = CAN_RIGHT
 
 if __name__ == "__main__":
-    piper = C_PiperInterface_V2()
+    piper = C_PiperInterface_V2(USE_CAN_NAME)
     piper.ConnectPort()
     piper.DisableArm(7)
     # piper.GripperCtrl(0, 1000, 0x02, 0)
